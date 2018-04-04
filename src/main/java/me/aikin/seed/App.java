bdf968b7cacb57d454e24f400ed9be17742958d9
aikin <1@aikin.me>
2018-04-04 13:19:36 +0800

    aikin: add lombok and spring boot

--- src/main/java/me/aikin/seed/App.java
+++ src/main/java/me/aikin/seed/App.java
@@ -1,11 +1,26 @@
 package me.aikin.seed;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@Slf4j
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        SpringApplication.run(App.class, args);
+
+        log.info("max memory:{} MB", Runtime.getRuntime().maxMemory() / 1024 / 1024);
+        log.info("total memory:{} MB", Runtime.getRuntime().totalMemory() / 1024 / 1024);
+        log.info("free memory:{} MB", Runtime.getRuntime().freeMemory() / 1024 / 1024);
+        log.info("used memory:{} MB", (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024);
+        log.info("available processors:{}", Runtime.getRuntime().availableProcessors());
     }
 }
