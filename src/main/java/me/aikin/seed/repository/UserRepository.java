a2147a603faca50414a71556ddcdbef779e01431
aikin <1@aikin.me>
2018-05-13 16:04:06 +0800

    aikin: add instert method

--- src/main/java/me/aikin/seed/repository/UserRepository.java
+++ src/main/java/me/aikin/seed/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package me.aikin.seed.repository;
 
 import me.aikin.seed.model.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserRepository {
     @Select("SELECT * FROM user WHERE id = #{userId}")
-    User findOne(Integer userId);
+    User findOne(Long userId);
+
+    @Insert("INSERT into user(ID, USERNAME, CREATED_AT) VALUES(#{id}, #{userName}, #{createdAt})")
+    void insert(User user);
 }
